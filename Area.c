c91bc774a765ab09ffd8d98cd037c875c0bddc29
sanaafrin89 <sanaafrin3712@gmail.com>
2025-08-25 16:11:43 +0530

    compute the p & a of a circle\

---
+++ Area.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main () {
+    float r;
+    float p;
+    float a;
+
+    printf("enter the radius: ");
+    scanf("%f",&r);
+
+    p=2*3.14*r;
+    a=3.14*r*r;
+
+
+    printf("perimeter of circle %f",p);<br>
+    printf("area of circle %f",a);
+
+
+
+
+}
